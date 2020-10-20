f9c73df93e8bd6c4858f372a2a6287b2da9ffb72
= <rh4618@ic.ac.uk>
2020-10-20 18:01:51 +0100

    Added framework for carma points on result of image taker page

--- lib/screens/image_taker_page/image_display_page.dart
+++ lib/screens/image_taker_page/image_display_page.dart
@@ -31,9 +31,10 @@ class ImageDisplayPageState extends State<ImageDisplayPage> {
   }
 
   void _detectText() async {
-    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(widget.image);
-    final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
-    final VisionText visionText = await textRecognizer.processImage(visionImage);
+    // final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(widget.image);
+    // final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
+    // final VisionText visionText = await textRecognizer.processImage(visionImage);
+    final visionText = null;
     setState(() {
       detectedTextWidget = _buildDetectedText(visionText);
     });
@@ -54,47 +55,80 @@ class ImageDisplayPageState extends State<ImageDisplayPage> {
     String origin;
 
     List<Widget> detectedTextBlocks = [
-      Image.file(widget.image),
-      Text("\n[Detected Text]"),
+      Container(
+        height: 250,
+        width: 250,
+        margin: EdgeInsets.only(
+            top: 50,
+            left: 20,
+            right: 20,
+            bottom: 20
+        ),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: FileImage(widget.image),
+          )
+        ),
+      ),
     ];
-    RegExp expSource = RegExp(r"MADE IN (\w+)");
-    RegExp expMaterial = RegExp(r"%\s?(\w+)");
+    // RegExp expSource = RegExp(r"MADE IN (\w+)");
+    // RegExp expMaterial = RegExp(r"%\s?(\w+)");
+    //
+    // for (int i = 0; i < visionText.blocks.length; i++) {
+    //   String text = visionText.blocks[i].text;
+    //   // detectedTextBlocks.add(
+    //   //     Text(text)
+    //   // );
+    //
+    //   if (origin == null) {
+    //     RegExpMatch originMatch = expSource.firstMatch(text.toUpperCase());
+    //     if (originMatch != null) {
+    //       origin = originMatch.group(0);
+    //       print("Source ${origin}");
+    //     }
+    //   }
+    //   if (material == null) {
+    //     RegExpMatch materialMatches = expMaterial.firstMatch(text.toUpperCase());
+    //     if (materialMatches != null) {
+    //       material = materialMatches.group(0);
+    //       print("Material ${material}");
+    //     }
+    //   }
+    // }
 
-    for (int i = 0; i < visionText.blocks.length; i++) {
-      String text = visionText.blocks[i].text;
-      detectedTextBlocks.add(
-          Text(text)
-      );
-
-      if (origin == null) {
-        RegExpMatch originMatch = expSource.firstMatch(text.toUpperCase());
-        if (originMatch != null) {
-          origin = originMatch.group(0);
-          print("Source ${origin}");
-        }
-      }
-      if (material == null) {
-        RegExpMatch materialMatches = expMaterial.firstMatch(text.toUpperCase());
-        if (materialMatches != null) {
-          material = materialMatches.group(0);
-          print("Material ${material}");
-        }
-      }
-    }
+    // detectedTextBlocks.addAll([
+    //   Text("Place of Manufacture: ${_cleanOriginText(origin)}"),
+    //   Text("Material: ${_cleanMaterialText(material)}\n")
+    // ]);
 
     detectedTextBlocks.addAll([
-      Text("\n\n[Detected Keywords]"),
-      Text("Place of Manufacture: ${_cleanOriginText(origin)}"),
-      Text("Material: ${_cleanMaterialText(material)}\n")
+      _CarmaPointDetails(points: -1),
+      _ClothingLabelDetail(
+            text: "Place of Manufacture: MYANMAR"
+      ),
+      _ClothingLabelDetail(
+          text: "Material: POLYESTER"
+      ),
+      Container(
+        padding: EdgeInsets.only(top: 30),
+        child: Center(
+          child: RaisedButton(
+            child: Text("Add to Closet"),
+          ),
+        ),
+      )
     ]);
     return Column(
-        children: [
-          Flexible(
-            child: ListView(
-              children: detectedTextBlocks,
-            )
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Flexible(
+          child: ListView(
+            children: detectedTextBlocks,
           )
-        ]
+        )
+      ]
     );
   }
 
@@ -105,8 +139,74 @@ class ImageDisplayPageState extends State<ImageDisplayPage> {
         title: Text('Recognised Text'),
       ),
       body: Center(
-            child: detectedTextWidget
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: detectedTextWidget
+        )
       ),
     );
   }
 }
+
+
+class _ClothingLabelDetail extends StatelessWidget {
+  final text;
+  _ClothingLabelDetail({this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Text(this.text)
+          ),
+        )
+    );
+  }
+}
+
+class _CarmaPointDetails extends StatefulWidget {
+  var points;
+  _CarmaPointDetails({this.points});
+
+  @override
+  _CarmaPointDetailsState createState() => _CarmaPointDetailsState();
+}
+
+class _CarmaPointDetailsState extends State<_CarmaPointDetails> {
+  Color carmaWidgetColour;
+  String carmaText;
+
+  @override
+  void initState() {
+    _getCarmaInfo();
+    super.initState();
+  }
+
+  void _getCarmaInfo() {
+    if (widget.points > 0) {
+      // Have some carma points
+      carmaWidgetColour = Colors.green;
+      carmaText = "${widget.points} Carma Points!";
+    } else {
+      // Display failed
+      carmaWidgetColour = Colors.red;
+      carmaText = "Oops. We can't get accurate readings, are the information correct?";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Center(
+              child: Text(carmaText, style: TextStyle(color: Colors.white))
+          ),
+        ),
+      color: carmaWidgetColour,
+    );
+  }
+}
+
