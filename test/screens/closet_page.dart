e7cafef33491913d9eab2c38e05cb682c59d79f2
yapjinxian <yapjinxian@gmail.com>
2020-10-27 15:40:15 +0800

    set up test files and basic structure

---
+++ test/screens/closet_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void closet_page_tests() {
+
+  test('Closet Test', () {
+    String test = "hehe";
+
+    expect(test, "hehe");
+  });
+}
--- test/testFile_test.dart
+++ test/testFile_test.dart
@@ -1,1 +1,17 @@
-// empty test file for testing purposes
+import 'package:flutter_test/flutter_test.dart';
+
+import 'screens/closet_page.dart';
+
+void main() {
+  test('Dummy Test', () {
+    int i = 0;
+    i++;
+    expect(i, 1);
+  });
+
+  group('Closet', closet_page_tests);
+
+  group('Item Dashboard', () {
+
+  });
+}
