29bb8ba5d7566b02e162e83b0941fbd93d67ab61
yapjinxian <yapjinxian@gmail.com>
2020-10-28 12:33:53 +0800

    added sample tests for closet page

--- test/screens/closet_page_tests.dart
+++ test/screens/closet_page_tests.dart
@@ -7,28 +7,36 @@ import 'package:flutter/material.dart';
 
 class MockClient extends Mock implements http.Client {}
 
+void catch_API_calls(http.Client client) {
+  when(client.get('https://us-central1-cfcalc.cloudfunctions.net/api/dummy'))
+      .thenAnswer((_) async => http.Response('"jaja"', 200));
+}
+
 void closet_page_tests() {
 
-  testWidgets('Closet Test', (WidgetTester tester) async {
+  testWidgets('Check closet page is loaded', (WidgetTester tester) async {
     final client = MockClient();
 
-    when(client.get('https://us-central1-cfcalc.cloudfunctions.net/api/dummy'))
-        .thenAnswer((_) async => http.Response('"jaja"', 200));
+    catch_API_calls(client);
 
     await tester.pumpWidget(MyApp(client));
+    await tester.tap(find.byIcon(Icons.sensor_window));
+    await tester.pumpAndSettle();
 
-    // await tester.tap(find.byIcon(Icons.sensor_window));
-
-
-    //
-    // await tester.pumpWidget(Closet(categories: ['cat1', 'cat2', 'cat3']));
-    //
-    // final categoriesFinder1 = find.text('cat1');
-    // final categoriesFinder2 = find.text('cat2');
-    // final categoriesFinder3 = find.text('cat3');
-    //
-    // expect(categoriesFinder1, findsOneWidget);
-    // expect(categoriesFinder2, findsOneWidget);
-    // expect(categoriesFinder3, findsOneWidget);
+    expect(find.text('Closet'), findsOneWidget);
   });
+
+  testWidgets('Check closet tab bar is loaded', (WidgetTester tester) async {
+    final client = MockClient();
+
+    catch_API_calls(client);
+
+    await tester.pumpWidget(MyApp(client));
+    await tester.tap(find.byIcon(Icons.sensor_window));
+    await tester.pump();
+
+    final tabsFinder = find.byType(TabBarView);
+    expect(tabsFinder, findsOneWidget);
+  });
+
 }
