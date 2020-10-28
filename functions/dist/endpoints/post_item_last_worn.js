80080bf49bfbcfda927daa1b4d72c6b484fbac28
Emily Haw <haw.emily@gmail.com>
2020-10-28 23:24:39 +0800

    remove some try and catch clauses to reduce repetition

--- functions/dist/endpoints/post_item_last_worn.js
+++ functions/dist/endpoints/post_item_last_worn.js
@@ -1,2 +1,13 @@
 "use strict";
+// import { Request, Response } from "express";
+// // import { database } from "firebase-admin";
+// export const wearItem = (
+//   req: Request,
+//   res: Response,
+//   db: FirebaseFirestore.Firestore
+// ) => {
+//   const { clothingID } = req.body;
+//   const closetRef = db.collection("closet");
+//   //   const clothingItem = closetRef.where('id');
+// };
 //# sourceMappingURL=post_item_last_worn.js.map
