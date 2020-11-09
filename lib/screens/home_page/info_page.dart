85d832daf07b3e24aadcf0a3d2a4a5f39cd533fb
dhivs <dr3818@ic.ac.uk>
2020-11-09 23:33:25 +0000

    Added information for all information pages except achievements

--- lib/screens/home_page/info_page.dart
+++ lib/screens/home_page/info_page.dart
@@ -35,7 +35,7 @@ class InformationPage extends StatelessWidget {
                                     textAlign: TextAlign.center),
                                 Padding(padding: EdgeInsets.only(top: 16.0)),
                                 Text(
-                                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                                    "Carma Points are what you accumulate in our application to help you track your progress and lessen your clothing carbon footprint. You can get carma points from lots of different ways such as wearing any clothing item in your closet, donating items after you've worn them the suggested number of times, or simply just donating them. Our aim is to use these points to keep users such as yourself accountable to your fast fashion decisions!",
                                     style: TextStyle(fontSize: 18),
                                     textAlign: TextAlign.center),
                               ]),
@@ -55,7 +55,7 @@ class InformationPage extends StatelessWidget {
                                   textAlign: TextAlign.center),
                               Padding(padding: EdgeInsets.only(top: 16.0)),
                               Text(
-                                  "It covers scanning a clothing label, adding items to the closet. Once in the closet, the user can access the item's dashboard page that has access to various information about the item such as material, Carma points, progress bar, etc. The user can choose to donate clothing items and can view the nearest donation centers near them.",
+                                  "Scanning an item is simple! Just click on the PLUS icon in the middle of the navigation bar at the bottom of the screen then take a picture (or upload a pre-taken pictue) of the clothing tag of the item you'd like to add to your closet. Our super cool fast fashion scanner will detect the materials used and the country the item was made in and all these numbers will contribute to calculating the number of times we recommend you to wear the item to make it's carbon footprint worth it!",
                                   style: TextStyle(fontSize: 18),
                                   textAlign: TextAlign.center),
                             ]),
@@ -76,7 +76,7 @@ class InformationPage extends StatelessWidget {
                                   textAlign: TextAlign.center),
                               Padding(padding: EdgeInsets.only(top: 16.0)),
                               Text(
-                                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                                  "After scanning an item and making sure all details are correct, simply press the ADD TO CLOSET button. This will add the clothing item to the closet and you can access it there at any point. If you've worn a clothing item and would like to track its usage (and earn some carma points for it), click on the picture of the item in the closet and tap the WEAR button to indicate that you've worn it an additional time. This should be shown in the circular progress bar around the picture of the item!",
                                   style: TextStyle(fontSize: 18),
                                   textAlign: TextAlign.center),
                             ]),
@@ -97,7 +97,7 @@ class InformationPage extends StatelessWidget {
                                   textAlign: TextAlign.center),
                               Padding(padding: EdgeInsets.only(top: 16.0)),
                               Text(
-                                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                                  "If you'd like to donate an individual item, simply click DONATE from the item's dashboard (accessed when clicking on the picture of an item in the closet). This will redirect you to a TO BE DONATED tab in the closet where you can select other items of clothes you'd like to donate. We will also prompt you to donate items of clothing that you've worn a sufficient amount of time or one you have not worn in a while (so others can get some use out of it). Once selecting all items to be donated, we will redirect you to the donations map, which will show you your location as well as the 5 nearest locations that you can donate your clothes at.",
                                   style: TextStyle(fontSize: 18),
                                   textAlign: TextAlign.center),
                             ]),
