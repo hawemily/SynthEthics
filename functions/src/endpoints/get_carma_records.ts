28fee950ce17107e15678b7be097e4d3e309881d
hyk3018 <47370661+hyk3018@users.noreply.github.com>
2020-11-22 23:31:22 +0000

    Changed format of result from /getCarmaRecord for ease of processing in the front end

--- functions/src/endpoints/get_carma_records.ts
+++ functions/src/endpoints/get_carma_records.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { Collections } from "../helper_components/db_collections";
 import { cleanCarmaRecord } from "../helper_components/update_carma_record";
 import { User } from "../models/users";
+import * as moment from "moment";
 
 export const getCarmaRecords = async (
   req: Request,
@@ -23,7 +24,27 @@ export const getCarmaRecords = async (
       await userRef.doc(userID).set(userData!);
 
       // Return carma record
-      res.status(200).send({carmaRecord : userData!["carmaRecord"]});
+      const result : any = {"days": [], "months": [], "years": []};
+
+      const dayRecords : {"day" : number, "value" : number}[] = userData!["carmaRecord"]["days"];
+      result["days"] = dayRecords.map(val => ({
+        "day" : moment(val["day"]).day(),
+        "value" : val["value"]
+      }));
+
+      const monthRecords : {"month" : number, "value" : number}[] = userData!["carmaRecord"]["months"];
+      result["months"] = monthRecords.map(val => ({
+        "month" : moment(val["month"]).month(),
+        "value" : val["value"]
+      }));
+
+      const yearRecords : {"year" : number, "value" : number}[] = userData!["carmaRecord"]["years"];
+      result["years"] = yearRecords.map(val => ({
+        "year" : moment(val["year"]).year(),
+        "value" : val["value"]
+      }));
+
+      res.status(200).send({carmaRecord : result});
     }
   } catch (e) {
       console.log(e);
