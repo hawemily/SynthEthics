e21be4c84636db4d4b0d27c57260f11ce91e1f00
Akanksha Sharma <aki@Akankshas-MBP.broadband>
2020-11-26 18:15:01 +0000

    Rendering Text Widget (toggle works)

--- lib/screens/dressing-room/randomOutfit.dart
+++ lib/screens/dressing-room/randomOutfit.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:synthetics/components/navbar/navbar.dart';
 import 'package:synthetics/responseObjects/clothingItemObject.dart';
 import 'package:synthetics/screens/closet_page/clothing_card.dart';
 import 'package:synthetics/services/api_client.dart';
@@ -18,29 +19,37 @@ class RandomOutfit extends StatefulWidget {
 }
 
 class _RandomOutfitState extends State<RandomOutfit> {
-  List<ClothingItemObject> randomItems = [];
+  Set<ClothingItemObject> randomItems = Set();
   int noOfItems = 2;
-  var types = new Set();
+  bool ref;
+  // var types = new Set();
 
   @override
   void initState() {
     super.initState();
-    randomItems = generateRandom();
+    this.ref = false;
+    this.randomItems = generateRandom(this.ref);
   }
 
-  // Future<File> getImage() {
-  //   return ImageManager.getInstance()
-  //       .loadPictureFromDevice(this.currentClothingItem.id);
-  // }
-
-  List<ClothingItemObject> generateRandom() {
+  Set<ClothingItemObject> generateRandom(bool ref) {
     var clothingItems = widget.clothingItems;
-    Random random = new Random();
-    clothingItems.forEach((key, value) {
-      randomItems.add(value[random.nextInt(value.length)]);
-    });
+    this.randomItems = Set();
+    print("Hiiiiiiiiiiiiiiiiiiiiii");
 
-    return randomItems;
+    // Random random = new Random();
+    clothingItems.forEach((key, value) {
+      // randomItems.add(value[random.nextInt(value.length)]);
+      if (!ref) {
+        value.forEach((element) {
+          if (element.data.brand == "Zara") this.randomItems.add(element);
+        });
+      } else {
+        value.forEach((element) {
+          if (element.data.brand == "Uqlo") this.randomItems.add(element);
+        });
+      }
+    });
+    return this.randomItems;
   }
 
   void saveOutfit() async {
@@ -70,58 +79,74 @@ class _RandomOutfitState extends State<RandomOutfit> {
     Navigator.pop(context);
   }
 
+  Widget clothingcardbuild() {
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: 0.8,
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      children: [
+        for (var item in this.randomItems)
+          () {
+            print("BUILD RENDER");
+            print(item.data.brand);
+            // return ClothingCard(clothingItem: item);
+            return Text(item.data.brand);
+          }()
+      ],
+    );
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(Object context) {
+    print("++++++++++++++++");
+    this.randomItems.forEach((element) {
+      print(element.data.brand);
+    });
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: CustomColours.greenNavy(),
-          iconTheme: IconThemeData(color: Colors.white),
-          title: Text('Generate Outfit'),
-        ),
-        body: ListView(
-          children: [
-            GridView.count(
-              crossAxisCount: 2,
-              childAspectRatio: 0.8,
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              children: [
-                for (var item in this.randomItems)
-                  () {
-                    return ClothingCard(clothingItem: item);
-                  }()
-              ],
-            ),
-            Padding(padding: EdgeInsets.all(20)),
-            ButtonBar(
-              alignment: MainAxisAlignment.spaceEvenly,
-              buttonHeight: 20,
-              buttonMinWidth: 60,
-              children: [
-                IconButton(
+      appBar: AppBar(
+        backgroundColor: CustomColours.greenNavy(),
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text('Generate Outfit'),
+      ),
+      body: Column(
+        children: [
+          clothingcardbuild(),
+          Padding(padding: EdgeInsets.all(20)),
+          ButtonBar(
+            alignment: MainAxisAlignment.spaceEvenly,
+            buttonHeight: 20,
+            buttonMinWidth: 60,
+            children: [
+              IconButton(
+                color: CustomColours.greenNavy(),
+                icon: Icon(Icons.close),
+                tooltip: 'Close',
+                onPressed: () => Navigator.pop(context),
+              ),
+              IconButton(
+                  icon: Icon(Icons.refresh),
                   color: CustomColours.greenNavy(),
-                  icon: Icon(Icons.close),
-                  tooltip: 'Close',
-                  onPressed: () => Navigator.pop(context),
-                ),
-                IconButton(
-                    icon: Icon(Icons.refresh),
-                    color: CustomColours.greenNavy(),
-                    tooltip: 'Refresh',
-                    onPressed: () => {
-                          setState(() {
-                            this.randomItems = generateRandom();
-                          })
-                        }),
-                IconButton(
-                  color: CustomColours.greenNavy(),
-                  icon: Icon(Icons.check),
-                  tooltip: 'Save',
-                  onPressed: () => saveOutfit(),
-                ),
-              ],
-            )
-          ],
-        ));
+                  tooltip: 'Refresh',
+                  onPressed: () => setState(() {
+                        this.ref = !this.ref;
+                        this.randomItems = this.generateRandom(!this.ref);
+                        print("------SETSTAE---------");
+                        this.randomItems.forEach((element) {
+                          print(element.data.brand);
+                        });
+                      })),
+              IconButton(
+                color: CustomColours.greenNavy(),
+                icon: Icon(Icons.check),
+                tooltip: 'Save',
+                onPressed: () => saveOutfit(),
+              ),
+            ],
+          )
+        ],
+      ),
+      bottomNavigationBar: NavBar(selected: 3),
+    );
   }
 }
