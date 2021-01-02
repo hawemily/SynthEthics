6c71b4d6d1600e466c6cedddc54165373ab635f9
Emily Haw <haw.emily@gmail.com>
2021-01-02 18:48:12 +0800

    delete user from firebase auth

--- lib/screens/home_page/widget/right_side_drawer.dart
+++ lib/screens/home_page/widget/right_side_drawer.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:synthetics/routes.dart';
@@ -7,7 +9,6 @@ import 'package:synthetics/services/current_user.dart';
 import 'package:synthetics/theme/custom_colours.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-
 class HomeRightDrawer extends StatefulWidget {
   @override
   _HomeRightDrawerState createState() => _HomeRightDrawerState();
@@ -15,99 +16,107 @@ class HomeRightDrawer extends StatefulWidget {
 
 class _HomeRightDrawerState extends State<HomeRightDrawer> {
   CurrentUser currUser = CurrentUser.getInstance();
+  FirebaseAuth auth = FirebaseAuth.instance;
 
   void _signOut() {
-    FirebaseAuth auth = FirebaseAuth.instance;
-
     if (currUser.signInMethod == SignInMethod.Google) {
       print("signing out from google!");
       currUser.googleSignIn.signOut();
     }
     auth.signOut().then((res) => {
-      Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(
-              builder: routes[routeMapping[Screens.Login]]),
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: routes[routeMapping[Screens.Login]]),
               (route) => false)
-    }
-    );
+        });
   }
+
+  void _deleteUser() {
+    api_client.post("/deleteUser",
+        body: jsonEncode(
+            <String, dynamic>{'uid': currUser.getUID()}));
+    auth.currentUser.delete().then((e){
+      print("delete successful from firebase auth frontend!");
+    }).catchError((e) {
+      print("ERROR WHEN DELETING USER");
+      print(e);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 200,
       child: Drawer(
-        child: Container(
-          color: CustomColours.greenNavy(),
-          padding: EdgeInsets.only(top: 100),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Container(
+          child: Container(
+        color: CustomColours.greenNavy(),
+        padding: EdgeInsets.only(top: 100),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
                 child: Center(
-                  child: Icon(Icons.eco, size: 50, color: CustomColours.offWhite(),)
-                )
-              ),
-              Container(
+                    child: Icon(
+              Icons.eco,
+              size: 50,
+              color: CustomColours.offWhite(),
+            ))),
+            Container(
                 padding: EdgeInsets.only(bottom: 50),
                 child: Center(
-                  child: Text("Synthethics", style: TextStyle(color: CustomColours.offWhite()),)
-                )
-              ),
-              _HomeRightDrawerItem(
-                // TODO: Remove this and implement functionalities to scanner
-                icon: Icons.format_paint,
-                text: "Colour Room",
-                onTap: () {
-                  Navigator.popUntil(
-                    context,
-                    ModalRoute.withName(routeMapping[Screens.Home])
-                  );
-                  Navigator.pushNamed(context, routeMapping[Screens.ColourRoom]);
-                },
-              ),
-              _HomeRightDrawerItem(
-                icon: Icons.accessibility,
-                text: "Accessibility",
-                onTap: () {
-                  print("Accessibility");
-                },
-              ),
-              _HomeRightDrawerItem(
-                icon: Icons.mobile_screen_share,
-                text: "Data Transfer",
-                onTap: () {
-                  print("Transfer Data");
-                },
-              ),
-              _HomeRightDrawerItem(
-                icon: Icons.exit_to_app,
-                text: "Logout",
-                onTap: () {
-                  _signOut();
-                },
-              ),
-              Expanded(
-                child: Align(
+                    child: Text(
+                  "Synthethics",
+                  style: TextStyle(color: CustomColours.offWhite()),
+                ))),
+            _HomeRightDrawerItem(
+              // TODO: Remove this and implement functionalities to scanner
+              icon: Icons.format_paint,
+              text: "Colour Room",
+              onTap: () {
+                Navigator.popUntil(
+                    context, ModalRoute.withName(routeMapping[Screens.Home]));
+                Navigator.pushNamed(context, routeMapping[Screens.ColourRoom]);
+              },
+            ),
+            _HomeRightDrawerItem(
+              icon: Icons.accessibility,
+              text: "Accessibility",
+              onTap: () {
+                print("Accessibility");
+              },
+            ),
+            _HomeRightDrawerItem(
+              icon: Icons.mobile_screen_share,
+              text: "Data Transfer",
+              onTap: () {
+                print("Transfer Data");
+              },
+            ),
+            _HomeRightDrawerItem(
+              icon: Icons.exit_to_app,
+              text: "Logout",
+              onTap: () {
+                _signOut();
+              },
+            ),
+            Expanded(
+              child: Align(
                   alignment: Alignment.bottomCenter,
                   child: _HomeRightDrawerItem(
                     icon: Icons.delete_forever,
                     text: "Delete Account",
                     onTap: () {
-                      api_client.post("/deleteUser/" + currUser.getUID());
+                      _deleteUser();
                       _signOut();
                     },
-                  )
-                ),
-              ),
-            ],
-          ),
-        )
-      ),
+                  )),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
-
 
 class _HomeRightDrawerItem extends StatefulWidget {
   final IconData icon;
@@ -131,11 +140,15 @@ class __HomeRightDrawerItemState extends State<_HomeRightDrawerItem> {
       ),
       margin: EdgeInsets.only(left: 10, right: 10, bottom: 5),
       child: ListTile(
-        leading: Icon(widget.icon, size: 30.0, color: CustomColours.greenNavy(),),
-        title: Text(widget.text, style: TextStyle(color: CustomColours.greenNavy())),
+        leading: Icon(
+          widget.icon,
+          size: 30.0,
+          color: CustomColours.greenNavy(),
+        ),
+        title: Text(widget.text,
+            style: TextStyle(color: CustomColours.greenNavy())),
         onTap: widget.onTap,
       ),
     );
   }
 }
-
